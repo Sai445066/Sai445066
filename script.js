c0ce642d8db48c1b14ad93f5d7acc6e9af14e561
Sai445066 <ramshetwadsai@gmail.com>
2024-07-15 16:36:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let score = 0;
+    const scoreDisplay = document.getElementById("score");
+    const clickButton = document.getElementById("clickButton");
+
+    clickButton.addEventListener("click", () => {
+        score++;
+        scoreDisplay.textContent = score;
+    });
+});
